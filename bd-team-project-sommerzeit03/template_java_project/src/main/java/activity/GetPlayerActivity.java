2bea685c5d7117682458425445d31b642ae19b9a
Leo Arellano <arellanoleonardo1996@gmail.com>
2023-07-11 14:15:17 -0400

    added code to GetPlayerActivity,Request,Result

--- bd-team-project-sommerzeit03/template_java_project/src/main/java/activity/GetPlayerActivity.java
+++ bd-team-project-sommerzeit03/template_java_project/src/main/java/activity/GetPlayerActivity.java
@@ -1,4 +1,50 @@
 package main.java.activity;
 
-public class GetPlayerActivity {
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import main.java.dynamodb.PlayerDao;
+import main.java.dynamodb.models.Player;
+import main.java.exceptions.PlayerNotFoundException;
+import main.java.models.requests.GetPlayerRequest;
+import main.java.models.results.GetPlayerResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class GetPlayerActivity implements RequestHandler<GetPlayerRequest, GetPlayerResult> {
+
+    private final Logger log = LogManager.getLogger();
+    private final PlayerDao playerDao;
+
+    public GetPlayerActivity() {
+        this.playerDao = new PlayerDao(new DynamoDBMapper(AmazonDynamoDBClient.builder()
+                .withRegion(Regions.US_WEST_2)
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+                .build()));
+    }
+
+    public GetPlayerActivity(PlayerDao playerDao) {
+        this.playerDao = playerDao;
+    }
+
+    /**
+     *
+     * @param getPlayerRequest The Lambda Function input
+     * @param context The Lambda execution environment context object.
+     * @return
+     */
+    @Override
+    public GetPlayerResult handleRequest(GetPlayerRequest getPlayerRequest, Context context) {
+        log.info("Received GetPlayerRequest {}", getPlayerRequest);
+        String requestedId = getPlayerRequest.getId();
+        Player player = playerDao.getPlayer(requestedId);
+        if (player == null) {
+            throw new PlayerNotFoundException();
+        }
+        // TODO: player model to return result
+        return null;
+    }
 }
--- bd-team-project-sommerzeit03/template_java_project/src/main/java/models/requests/GetPlayerRequest.java
+++ bd-team-project-sommerzeit03/template_java_project/src/main/java/models/requests/GetPlayerRequest.java
@@ -1,4 +1,61 @@
 package main.java.models.requests;
 
+import java.util.Objects;
+
 public class GetPlayerRequest {
+
+    private String id;
+
+    public GetPlayerRequest() {
+
+    }
+
+    public GetPlayerRequest(Builder builder) {
+        this.id = builder.id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GetPlayerRequest)) return false;
+        GetPlayerRequest that = (GetPlayerRequest) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "GetPlayerRequest{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private String id;
+
+        public Builder withId(String idToUse) {
+            this.id  = idToUse;
+            return this;
+        }
+
+        public GetPlayerRequest build() {
+            return new GetPlayerRequest(this);
+        }
+    }
 }
